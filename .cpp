06007bd9026d70585dc187a69ed5c3fffabb1933
OmarKhalifa2k5 <146870960+OmarKhalifa2k5@users.noreply.github.com>
2024-03-13 22:04:10 +0200

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	char x[6][6] = {
+	{'a','b','c','d','e','f'},
+	{'g','h','i','j','k','l'},
+	{'m','n','o','p','q','r'},
+	{'s','t','u','v','w','x'},
+	{'y','z','1','2','3','4'},
+	{'5','6','7','8','9','0'}
+	};
+	
+	char letter;
+	int n_letter;
+	int col;
+	int row;
+	
+	cin >> letter;
+	//first_letter = letter - 96;
+	n_letter = letter - 96;
+	
+	
+	return 0;
+}
